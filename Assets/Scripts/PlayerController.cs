5ed99d0b4aa4c254dcdd92ab5a641fe5a9ad4488
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-09-01 22:52:55 -0700

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
+    public int speed = 15;
     // Start is called before the first frame update
     void Start()
     {
